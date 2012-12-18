fb4f18b5a27a4be78a3976d855052ada7fbe6216
Ritooon <henri.vignaux@gmail.com>
2012-12-18 10:33:33 +0100

    Delete PORT information
    I n pdo_mysql.class.php

--- application/models/pdo_mysql.class.php
+++ application/models/pdo_mysql.class.php
@@ -11,7 +11,7 @@ class PDO_Mysql
     function init() {
         try {
             self::$_instancePDO  =  new PDO(
-                    'mysql:host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_NAME, DB_USER, DB_PASSWORD, 
+                    'mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASSWORD, 
                     array(PDO::ATTR_PERSISTENT => true, 
                           PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\'')
                     );
